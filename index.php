b7aa117bc20de5d6e6b538d5135a08891be76119
phatneglo <phatneglo@yahoo.com>
2020-09-08 14:01:41 +0800

    Modifed

--- index.php
+++ index.php
@@ -1,28 +1,33 @@
 <?php
-    if( $_SERVER['REQUEST_METHOD']=='POST' && isset( $_POST['image'],$_POST['filename'] ) ){
+
+  if( $_SERVER['REQUEST_METHOD']=='POST' && isset( $_POST['image'],$_POST['filename'] ) ){
 
         $image=$_POST['image'];
+        // change your directory path here
+        $savepath='./img-uploads/'; 
+        // change the final format if you wish
         $filename=$_POST['filename'];
-
-       
-        $savepath='./uploads/'; #put your path here
-
         $target=$savepath . $filename;
 
         $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $image));
         $result=file_put_contents( $target,  $data  );
 
-
-        header('HTTP/1.1 200 OK',true,200);
-        header('Content-Type: text/plain');
-        exit( $result ? $filename : sprintf( 'Unable to save %s',$filename ) );
+        if ($result) {
+            die (json_encode([
+                'result' => 'Success',
+                'filename' => $filename,
+                'save_path' => $target
+            ]));
+        } else {
+            die (json_encode(['result' => 'Failed', 'error' => var_dump ($result)]));
+        }
     }
 ?>
 <!DOCTYPE html>
 <html>
 
 <body>
-    <form action="img-upload.php" method="post" enctype="multipart/form-data">
+    <form action="index.php" method="post" enctype="multipart/form-data">
         Select image to upload:
         <input type="file" name="fileToUpload" id="attachmentFile" accept="image/x-png,image/gif,image/jpeg">
         <input type="submit" value="Upload Image" name="submit">
@@ -30,14 +35,18 @@
     </form>
     <script>
     document.getElementById("attachmentFile").addEventListener("change", function(event) {
-        compress(event);
+        JSImageCompress(event);
     });
 
-    function compress(e) {
-        // SET MO UNG FILE DITO
-        const width = 800;
-        const height = 640;
-        const fileName = e.target.files[0].name;
+
+    function JSImageCompress(e) {
+        const width = 500;
+
+        const fileField = e.target.files[0];
+        const fileName = fileField.name;
+
+        if (fileName == "") return false;
+
         const reader = new FileReader();
         reader.readAsDataURL(e.target.files[0]);
         reader.onload = event => {
@@ -45,50 +54,51 @@
             img.src = event.target.result;
 
             img.onload = () => {
-                    const elem = document.createElement('canvas');
-                    elem.width = width;
-                    elem.height = height;
-                    const ctx = elem.getContext('2d');
-                    // img.width and img.height will contain the original dimensions
-                    ctx.drawImage(img, 0, 0, width, height);
-                    ctx.canvas.toBlob((blob) => {
-                        const file = new File([blob], fileName, {
-                            type: 'image/jpeg',
-                            lastModified: Date.now()
-                        });
-                    }, 'image/jpeg', 1);
+                const elem = document.createElement('canvas');
+                const scaleFactor = width / img.width;
+                elem.width = width;
+                elem.height = img.height * scaleFactor;
 
-                    console.log(ctx.canvas.toDataURL('image/jpeg'));
-                    var fd = new FormData();
-                    fd.append('action', 'save');
-                    fd.append('image', ctx.canvas.toDataURL('image/jpeg'));
-                    fd.append('filename', fileName)
+                // Create a 2d canvass element to hold the image
+                const ctx = elem.getContext('2d');
+
+                ctx.drawImage(img, 0, 0, width, img.height * scaleFactor);
+                ctx.canvas.toBlob((blob) => {
+                    const file = new File([blob], fileName, {
+                        type: 'image/jpeg',
+                        lastModified: Date.now()
+                    });
+
+                }, 'image/jpeg', 1);
 
 
-                    var ajax = function(url, data, callback) {
-                        var xhr = new XMLHttpRequest();
-                        xhr.onreadystatechange = function() {
-                            if (this.readyState == 4 && this.status == 200) callback.call(this, this
-                                .response);
-                        };
-                        xhr.open('POST', url, true);
-                        xhr.send(data);
-                    };
+                let formData = new FormData();
 
-                    var callback = function(r) {
-                        // ALERT MO DITO FOR TESTING
-                        // alert(r)
-                        document.getElementById('imgfilename').value = r;
+                formData.append("filename", "resized" + fileName);
+                formData.append("image", ctx.canvas.toDataURL('image/jpeg'));
 
+                (async () => {
+                    let response = await fetch('index.php', {
+                        method: 'POST',
+                        body: formData
+                    });
+                    let result = await response.json();
+                    console.log(result);
+
+                    if (result.result == 'Success') {
+                        document.getElementById('attachmentFile').value = '';
+                        document.getElementById('imgfilename').value = result.filename;
+                        alert("Resized and Uploaded in " + result.save_path);
+                    } else {
+                        document.getElementById('attachmentFile').value = '';
+                        alert(result.error);
                     }
 
-                    ajax.call(this, location.href, fd, callback);
+                })();
 
 
-
-                },
-                reader.onerror = error => console.log(error);
-        };
+            }
+        }
     }
     </script>
 </body>
